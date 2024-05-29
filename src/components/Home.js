b47d67f383e2ad817084381b1c4e17aa78269b60
Rihab <rihab.arioui@gmail.com>
2024-05-29 22:19:05 +0100

    feat: Create basic UI for Home component

---
+++ src/components/Home.js
@@ -0,0 +1,42 @@
+import React from "react";
+import logo from "../images/logo.svg";
+import triangle from "../images/bg-triangle.svg";
+import rock from "../images/icon-rock.svg";
+import scissors from "../images/icon-scissors.svg";
+import paper from "../images/icon-paper.svg";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-Background grid place-items-center grid-rows-[1fr_2fr]">
+      <div className="w-full h-full grid place-items-center">
+        <div className="w-1/2 h-2/3 grid grid-cols-2 border-4 border-white rounded-2xl p-10">
+          <img className="place-self-start h-full" src={logo} alt="logo" />
+          <div className="place-self-end grid grid-rows-[1fr_2fr] place-items-center bg-white rounded-xl h-full w-1/3 p-5">
+            <p>SCORE</p>
+            <h3 className=" text-5xl">12</h3>
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-full grid place-items-center">
+        <div className="w-1/2 h-full grid place-items-center grid-cols-2 grid-rows-2">
+          <img className="fixed" src={triangle} alt="bg-rectangle" />
+          <button className="z-0 bg-primary-Paper rounded-full p-5">
+            <div className="w-fit h-fit p-10 rounded-full bg-white">
+              <img src={paper} alt="paper" />
+            </div>
+          </button>
+          <button className="z-0 bg-primary-Scissors rounded-full p-5">
+            <div className="w-fit h-fit p-10 rounded-full bg-white">
+              <img src={scissors} alt="scissors" />
+            </div>
+          </button>
+          <button className="z-0 col-start-1 col-end-3 bg-primary-Rock rounded-full p-5">
+            <div className="w-fit h-fit p-10 rounded-full bg-white">
+              <img src={rock} alt="rock" />
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          Scissors: "hsl(39, 89%, 49%)",
+          Paper: "hsl(230, 89%, 62%)",
+          Rock: "hsl(349, 71%, 52%)",
+        },
+        Neutral: {
+          Dark: "hsl(229, 25%, 31%)",
+          Score: "hsl(229, 64%, 46%)",
+          Header: "hsl(217, 16%, 45%)",
+        },
+        Background: "hsl(214, 47%, 23%)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
