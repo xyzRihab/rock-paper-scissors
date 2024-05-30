215916039abf187e76018199336785e43cc30e6c
Rihab <rihab.arioui@gmail.com>
2024-05-30 11:59:50 +0100

    feat: Created Result route

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./../components/Home";
+import Result from "../components/Result";
 
 export default function Router() {
   return (
@@ -8,6 +9,7 @@ export default function Router() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/result" element={<Result />} />
         </Routes>
       </BrowserRouter>
     </div>
