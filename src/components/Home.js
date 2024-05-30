22b9212374f4a07dd0efef0ee577e16a3c752c4b
Rihab <rihab.arioui@gmail.com>
2024-05-30 12:02:36 +0100

    feat: Add navigation and choice handling in Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,15 @@ import closeBtn from "../images/icon-close.svg";
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
 
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
+  const navigate = useNavigate();
+
+  const handleChoice = (selectedChoice) => {
+    navigate("./result", { state: { choice: selectedChoice } });
+  };
+
   return (
     <div className="w-screen h-screen bg-Background grid place-items-center grid-rows-[1fr_2fr]">
       <div className="w-full h-full grid place-items-center">
@@ -25,17 +33,26 @@ export default function Home() {
       <div className="w-full h-full grid place-items-center">
         <div className="w-1/2 h-full grid place-items-center grid-cols-2 grid-rows-2">
           <img className="fixed" src={triangle} alt="bg-rectangle" />
-          <button className="z-0 bg-primary-Paper rounded-full p-5">
+          <button
+            onClick={() => handleChoice("paper")}
+            className="z-0 bg-primary-Paper rounded-full p-5"
+          >
             <div className="w-fit h-fit p-10 rounded-full bg-white">
               <img src={paper} alt="paper" />
             </div>
           </button>
-          <button className="z-0 bg-primary-Scissors rounded-full p-5">
+          <button
+            onClick={() => handleChoice("scissors")}
+            className="z-0 bg-primary-Scissors rounded-full p-5"
+          >
             <div className="w-fit h-fit p-10 rounded-full bg-white">
               <img src={scissors} alt="scissors" />
             </div>
           </button>
-          <button className="z-0 col-start-1 col-end-3 bg-primary-Rock rounded-full p-5">
+          <button
+            onClick={() => handleChoice("rock")}
+            className="z-0 col-start-1 col-end-3 bg-primary-Rock rounded-full p-5"
+          >
             <div className="w-fit h-fit p-10 rounded-full bg-white">
               <img src={rock} alt="rock" />
             </div>
